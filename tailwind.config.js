d1c79a58bd6852c17cba2a528d9a7bbe8f4cca9d
ProjectsOf-Dashu <hitechgames06@gmail.com>
2024-07-21 09:43:51 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+       
+        fontFamily:{
+          
+          'custom' : ['poppins','sans-serif'],
+        },
+
+        screens:{
+       
+           mysm:'440px',
+ 
+        }
+    },
+  },
+  plugins: [],
+}
+
